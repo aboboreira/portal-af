7e7f343c526d9afaf12d781a75a2595e748568b8
INDRA\gferreiram <gferreiram@indracompany.com>
2018-09-21 20:44:42 -0300

    criação de header.php, sidebar.php, css e pagina principal de usuário atividades

---
+++ application/views/atividade_home.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+    <title>Home Atividades</title>
+</head>
+<body>
+    <!--INSERIR HEADER-->
+    <section id="icon-boxes" class="py-5">
+        <div class="row justify-content-between">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="info-header mb-5">
+                        <h1 class="color-blue pb-3">
+                            Atividades Recentes
+                        </h1>
+                    </div>
+                </div>
+                <div class="row testa container">
+                    <div class="row center-div clearfix w-100">
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange mb-resp text-white">
+                                    <div class="card-body">
+                                        <i class="fa fa-mortar-board fa-3x"></i>
+                                        <p>Atividade</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange mb-resp text-white">
+                                    <div class="card-body">
+                                        <i class="fa fa-mortar-board fa-3x"></i>
+                                        <p>Atividade</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange mb-resp text-white">
+                                    <div class="card-body">
+                                        <i class="fa fa-mortar-board fa-3x"></i>
+                                        <p>Atividade</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div class="row mt-5 w-100">
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange mb-resp text-white">
+                                    <div class="card-body">
+                                        <i class="fa fa-mortar-board fa-3x"></i>
+                                        <p>Atividade</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange mb-resp text-white">
+                                    <div class="card-body">
+                                        <i class="fa fa-mortar-board fa-3x"></i>
+                                        <p>Atividade</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange text-white">
+                                    <div class="card-body">
+                                        <i class="fa fa-plus-square fa-3x"></i>
+                                        <p>Atividade</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="info-header mb-5">
+                        <h1 class="color-blue pb-3">
+                            Minhas Conquistas  
+                        </h1>
+                    </div>
+                </div>
+                <div class="row testa p-4">
+                    <div class="row w-100">
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange text-white mb-resp">
+                                    <div class="card-body">
+                                        <i class="fa fa-star-o fa-3x"></i>
+                                        <p>Conquista</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange text-white mb-resp">
+                                    <div class="card-body">
+                                        <i class="fa fa-star-o fa-3x"></i>
+                                        <p>Conquista</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange text-white mb-resp">
+                                    <div class="card-body">
+                                        <i class="fa fa-star-o fa-3x"></i>
+                                        <p>Conquista</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div class="row mt-5 w-100">
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card text-center color-orange text-white mb-resp">
+                                    <div class="card-body">
+                                        <i class="fa fa-star-o fa-3x"></i>
+                                        <p>Conquista</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card color-orange text-center text-white mb-resp">
+                                    <div class="card-body">
+                                        <i class="fa fa-star-o fa-3x"></i>
+                                        <p>Conquista</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#">
+                                <div class="card color-orange text-center text-white mb-resp">
+                                    <div class="card-body">
+                                        <i class="fa fa-star-o fa-3x"></i>
+                                        <p>Conquista</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
